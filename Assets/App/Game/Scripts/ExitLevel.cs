87653cc8f0a1ff510a5b3b198343cb717be9ea7e
Matt F <matomatical@gmail.com>
2016-08-25 16:17:46 +1000

    finish line and exit level

---
+++ Assets/App/Game/Scripts/ExitLevel.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ExitLevel : MonoBehaviour {
+	
+	public UIHelper uihelp;
+
+	void OnTriggerEnter2D(Collider2D other) {
+		if (other.gameObject.CompareTag ("Player")) {
+			uihelp.iGoTo ("PostGameMenu");
+		}
+	}
+}
---
+++ Assets/App/Game/Scripts/FinishLine.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FinishLine : MonoBehaviour {
+
+	public ClockController clock;
+
+	void OnTriggerEnter2D (Collider2D other) {
+		if (other.gameObject.CompareTag ("Player")) {
+			clock.StopTiming ();
+		}
+	}
+}
