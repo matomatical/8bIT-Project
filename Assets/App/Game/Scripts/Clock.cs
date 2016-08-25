07a2bcffef10e85d376abbec15fdd8feacf8e007
Matt F <matomatical@gmail.com>
2016-08-25 12:28:49 +1000

    level clock

---
+++ Assets/App/Game/Scripts/Clock.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class Clock : MonoBehaviour {
+
+	float time = 0;
+	bool timing = false;
+
+	private Text label; // the text object we're attached to
+
+	void Start () {
+		label = GetComponent<Text> ();
+	}
+
+	// Update is called once per frame
+	void Update () {
+		
+		if(timing){
+
+			// count
+			time += Time.deltaTime;
+
+			// convert to clock time string
+			int mins = (int) (time / 60);
+			int secs = ((int) time) % 60;
+			string text = string.Format("{0:00}:{1:00}", mins, secs);
+
+			// lazy refresh text
+			if(!(label.text == text)){
+				label.text = text;
+			}
+		}
+	}
+
+	public void Stop(){
+		timing = false;
+	}
+
+	public void Start(){
+		timing = true;
+	}
+}
