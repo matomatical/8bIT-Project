273b109574cadcb0629503145566b20b2012f75a
U-MARIAM\mariam <mariam>
2016-08-27 14:06:18 +1000

    Added in basic matchmaking - establishing connection to a room

---
+++ Assets/App/UI/MultiPlayerController.cs
@@ -0,0 +1,106 @@
+﻿using UnityEngine;
+using System.Collections;
+using GooglePlayGames;
+using GooglePlayGames.BasicApi.Multiplayer;
+using System;
+
+// Making this controller the listener as well to keep all the MultiPlayer logic in one class
+public class MultiplayerController : GooglePlayGames.BasicApi.Multiplayer.RealTimeMultiplayerListener
+{
+    public MatchMaking room;
+
+    // Making this a singleon as it'll be used in both the main menu and the game
+    private static MultiplayerController _instance = null;
+
+    // Sticking to a 2 player game
+    private uint minimumPartners = 1;
+    private uint maximumPartners = 1;
+
+    // Sticking with a single game mode
+    private uint gameVariation = 0;
+
+    private MultiplayerController()
+    {
+        PlayGamesPlatform.DebugLogEnabled = true;
+        PlayGamesPlatform.Activate();
+    }
+
+    public static MultiplayerController Instance
+    {
+        get
+        {
+            if (_instance == null)
+            {
+                _instance = new MultiplayerController();
+            }
+            return _instance;
+        }
+    }
+
+    public void StartMPGame()
+    {
+        StartMatchMaking();
+    }
+
+    private void StartMatchMaking()
+    {
+        PlayGamesPlatform.Instance.RealTime.CreateQuickGame(minimumPartners, maximumPartners, gameVariation, this);
+    }
+
+    private void ShowMPStatus(string message)
+    {
+        Debug.Log(message);
+        if (room != null)
+        {
+            room.SetRoomStatusMessage(message);
+        }
+    }
+
+    public void OnRoomSetupProgress(float percent)
+    {
+        ShowMPStatus("We are " + percent + "% done with setup");
+    }
+
+    public void OnRoomConnected(bool success)
+    {
+        if (success)
+        {
+            ShowMPStatus("We are connected to the room! WOOHOO!");
+        }
+        else
+        {
+            ShowMPStatus("Uh-oh. Looks like something went wrong when trying to connect to the room.");
+        }
+    }
+
+    public void OnLeftRoom()
+    {
+        ShowMPStatus("We have left the room. We should probably perform some clean-up stuff.");
+    }
+
+    public void OnParticipantLeft(Participant participant)
+    {
+        ShowMPStatus("Player " + participant.DisplayName + " has left.");
+    }
+
+    public void OnPeersConnected(string[] participantIds)
+    {
+        foreach (string participantID in participantIds)
+        {
+            ShowMPStatus("Player " + participantID + " has joined.");
+        }
+    }
+
+    public void OnPeersDisconnected(string[] participantIds)
+    {
+        foreach (string participantID in participantIds)
+        {
+            ShowMPStatus("Player " + participantID + " has left.");
+        }
+    }
+
+    public void OnRealTimeMessageReceived(bool isReliable, string senderId, byte[] data)
+    {
+        ShowMPStatus("We have received some gameplay messages from participant ID:" + senderId);
+    }
+}
---
+++ Assets/App/UI/Views/MatchMaking.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+using GooglePlayGames;
+
+public class MatchMaking : MonoBehaviour {
+    // Holds the image that will be the background of the dialogue box
+    public GUISkin guiSkin;
+
+    // Whether to display the room dialogue box and what message it should contain
+    private bool showRoomDialogue;
+    private string roomMessage;
+
+	void Start()
+    {
+        showRoomDialogue = false;
+        PlayGamesPlatform.Activate();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Debug.Log("here");
+    }
+
+    void OnGUI () {
+        if (!showRoomDialogue)
+        {
+            roomMessage = "Starting a multi-player game...";
+            showRoomDialogue = true;
+            MultiplayerController.Instance.room = this;
+            MultiplayerController.Instance.StartMPGame();
+        }
+        
+        if (showRoomDialogue)
+        {
+            GUI.skin = guiSkin;
+            GUI.Box(new Rect(Screen.width * 0.25f, Screen.height * 0.4f, Screen.width * 0.5f, Screen.height * 0.5f), roomMessage);
+        }
+    }
+
+    public void SetRoomStatusMessage(string message)
+    {
+        roomMessage = message;
+    }
+
+    // Instructs the title Screen to
+    public void HideRoom()
+    {
+        roomMessage = "";
+        showRoomDialogue = false;
+    }
+}
