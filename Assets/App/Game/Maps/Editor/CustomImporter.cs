94411d31a24cf673de7f7453203afdfa685d1225
Athir Saleem <noc7c9@gmail.com>
2016-08-31 07:21:33 +1000

    implemented keys and doors

---
+++ Assets/App/Game/Maps/Editor/CustomImporter.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using Tiled2Unity;
+using System.Collections.Generic;
+
+[CustomTiledImporter]
+class CustomImporter : ICustomTiledImporter
+{
+	GameObject keyPrefab = (GameObject)Resources.Load("Key");
+	GameObject keyBlockPrefab = (GameObject)Resources.Load("KeyBlock");
+
+    public void HandleCustomProperties(GameObject marker,
+        IDictionary<string, string> keyValuePairs)
+	{
+        if (keyValuePairs.ContainsKey("Key")) {
+			replaceMarker(marker, keyPrefab);
+		} else if (keyValuePairs.ContainsKey("KeyBlock")) {
+			replaceMarker(marker, keyBlockPrefab);
+        }
+    }
+
+    public void CustomizePrefab(GameObject prefab)
+    {
+    	// do nothing
+    }
+
+	GameObject replaceMarker(GameObject marker, GameObject prefab) {
+		GameObject newObject = GameObject.Instantiate(prefab);
+		newObject.transform.parent = marker.transform.parent.transform;
+		newObject.transform.position = marker.transform.position;
+
+		GameObject.DestroyImmediate(marker.GetComponent<BoxCollider2D>());
+		marker.name = "__to_delete__";
+
+		return newObject;
+    }
+}
---
+++ Assets/App/Game/Scripts/KeyHolder.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class KeyHolder : MonoBehaviour {
+
+	public bool holdingKey = false;
+
+}
