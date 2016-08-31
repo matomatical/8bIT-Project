5f26ea493282ac9206ce26dc11ca150227633876
Matt F <matomatical@gmail.com>
2016-08-31 20:21:59 +1000

    broken record/rewatch

---
+++ Assets/RecordRewatchTest/SerializeController.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Team8bITProject {
+	
+	public class SerializeController : MonoBehaviour {
+
+		public float fps = 30;
+		public Serializer[] objects;
+
+		public DeserializeController ds;
+
+		void Start() {
+
+		}
+
+		// how to control the time of this? not sure
+
+		void FixedUpdate () {
+
+			string frame = "";
+
+			foreach (Serializer s in objects){
+				frame += s.Serialize () + ";";
+			}
+
+			ds.Send (frame);
+		}
+	}
+}
