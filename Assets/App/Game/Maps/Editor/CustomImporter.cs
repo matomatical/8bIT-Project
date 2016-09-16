ea58ca122b33daf3afa1264bfaa8356df4be16b3
Athir Saleem <noc7c9@gmail.com>
2016-09-17 00:46:59 +1000

    added finish line and exit colliders to tiled importer

--- Assets/App/Game/Maps/Editor/CustomImporter.cs
+++ Assets/App/Game/Maps/Editor/CustomImporter.cs
@@ -3,36 +3,45 @@ using Tiled2Unity;
 using System.Collections.Generic;
 
 [CustomTiledImporter]
-class CustomImporter : ICustomTiledImporter
-{
+class CustomImporter : ICustomTiledImporter {
+
+	Vector3 offset = new Vector3(0.5f, -0.5f, 0);
+
 	GameObject keyPrefab = (GameObject)Resources.Load("Key");
 	GameObject keyBlockPrefab = (GameObject)Resources.Load("KeyBlock");
 	GameObject pressurePlatePrefab = (GameObject)Resources.Load("PressurePlate");
 	GameObject pressurePlateBlockPrefab = (GameObject)Resources.Load("PressurePlateBlock");
+	GameObject finishLinePrefab = (GameObject)Resources.Load("FinishLine");
+	GameObject exitPrefab = (GameObject)Resources.Load("Exit");
 
     public void HandleCustomProperties(GameObject marker,
-        IDictionary<string, string> props)
-	{
+        IDictionary<string, string> props) {
         if (props.ContainsKey("Key")) {
 			replaceMarker(marker, keyPrefab);
 		} else if (props.ContainsKey("KeyBlock")) {
 			replaceMarker(marker, keyBlockPrefab);
 		} else if (props.ContainsKey("PressurePlate")) {
-			replaceMarker(marker, pressurePlatePrefab).GetComponent<PressurePlate>().address = props["PressurePlate"];
+			GameObject plate = replaceMarker(marker, pressurePlatePrefab);
+			plate.GetComponent<PressurePlate>().address = props["PressurePlate"];
 		} else if (props.ContainsKey("PressurePlateBlock")) {
-			replaceMarker(marker, pressurePlateBlockPrefab).GetComponent<PressurePlateBlock>().address = props["PressurePlateBlock"];
-        }
+			GameObject block = replaceMarker(marker, pressurePlateBlockPrefab);
+			block.GetComponent<PressurePlateBlock>().address = props["PressurePlateBlock"];
+        } else if (props.ContainsKey("FinishLine")) {
+			replaceMarker(marker, finishLinePrefab);
+		} else if (props.ContainsKey("Exit")) {
+			replaceMarker(marker, exitPrefab);
+		}
     }
 
-    public void CustomizePrefab(GameObject prefab)
-    {
+    public void CustomizePrefab(GameObject prefab) {
     	// do nothing
     }
 
 	GameObject replaceMarker(GameObject marker, GameObject prefab) {
 		GameObject newObject = GameObject.Instantiate(prefab);
 		newObject.transform.parent = marker.transform.parent.transform;
-		newObject.transform.position = marker.transform.position;
+
+		newObject.transform.position = offset + marker.transform.position;
 
 		GameObject.DestroyImmediate(marker.GetComponent<BoxCollider2D>());
 		marker.name = "__ignored__";
--- Assets/App/Game/Scripts/FinishLine.cs
+++ Assets/App/Game/Scripts/FinishLine.cs
@@ -6,7 +6,7 @@ public class FinishLine : MonoBehaviour {
 	public ClockController clock;
 
 	void OnTriggerEnter2D (Collider2D other) {
-		if (other.gameObject.CompareTag ("Player")) {
+		if (clock != null && other.gameObject.CompareTag("Player")) {
 			clock.StopTiming ();
 		}
 	}
