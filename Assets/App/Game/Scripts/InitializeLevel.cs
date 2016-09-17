fa57f71460001526b239e970e5652b7b415dbdc8
Athir Saleem <noc7c9@gmail.com>
2016-09-17 19:09:16 +1000

    dynamic level loading is now possible

--- Assets/App/Game/Scripts/InitializeLevel.cs
+++ Assets/App/Game/Scripts/InitializeLevel.cs
@@ -1,26 +1,32 @@
 ﻿using UnityEngine;
+using Tiled2Unity;
 using System.Collections;
 
 public class InitializeLevel : MonoBehaviour {
 
-	new public GameObject camera;
+	new public CameraController camera;
 	public GameObject gui;
 	public GameObject level;
 
-	void Start () {
-		GameObject objects = GameObject.Find("Objects");
+	void Awake() {
+		GameObject prefab = Maps.Load("Test_Level_3");
+		if (prefab) {
+			level = GameObject.Instantiate<GameObject>(prefab);
+			camera.level = level.GetComponent<TiledMap>();
+		}
+	}
 
+	void Start () {
 		// finish line should have a reference to the clock
-		FinishLine finishLine = objects.GetComponentInChildren<FinishLine>();
+		FinishLine finishLine = level.GetComponentInChildren<FinishLine>();
 		finishLine.clock = gui.GetComponentInChildren<ClockController>();
 
 		// player should have a reference to the joystick
-		PlayerController player = objects.GetComponentInChildren<PlayerController>();
+		PlayerController player = level.GetComponentInChildren<PlayerController>();
 		player.joystick = gui.GetComponentInChildren<Joystick>();
 
 		// camera should have a reference to the player
-		CameraController cameraController = camera.GetComponent<CameraController>();
-		cameraController.follow = player.gameObject.transform;
+		camera.follow = player.gameObject.transform;
 	}
 
 }
---
+++ Assets/App/Game/Scripts/Maps.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class Maps {
+
+	public static string[] maps = new string[] {
+		"Test_Level_1",
+		"Test_Level_2",
+		"Test_Level_3"
+	};
+
+	public static GameObject Load(string name) {
+		return Resources.Load("Maps/" + name) as GameObject;
+	}
+
+}
