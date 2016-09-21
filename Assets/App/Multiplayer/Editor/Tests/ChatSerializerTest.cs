bfc47d3cb649a68acd3c97ac7902402b40542257
U-MARIAM\mariam <mariam>
2016-09-21 15:31:21 +1000

    Adding in tests for chat

---
+++ Assets/App/Multiplayer/Editor/Tests/ChatSerializerTest.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace xyz._8bITProject.cooperace.multiplayer.tests {
+    [TestFixture]
+    public class ChatSerializerTest : MonoBehaviour {
+
+        [Test]
+        public void SerializeDserialize() {
+            ChatController serializer = new ChatController();
+
+            // Serialize some information
+            PlayerInformation info = new PlayerInformation(0, 0, PlayerInformation.PlayerState.MOVING);
+            List<byte> data = serializer.Serialize(info);
+
+            // Deserialize the result
+            PlayerInformation resultingInfo = serializer.Deserialize(data);
+
+            // Check the information is the same as the original
+            Assert.That(resultingInfo.Equals(info));
+        }
+    }
+}
---
+++ Assets/App/Multiplayer/Editor/Tests/PlayerSerializerTest.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using NUnit.Framework;
+
+namespace xyz._8bITProject.cooperace.multiplayer.tests
+{
+	[TestFixture]
+	public class PlayerSerializerTest
+	{
+		[Test]
+		public void SerializeDserialize () {
+			PlayerSerializer serializer = new PlayerSerializer ();
+
+			// Serialize some information
+			PlayerInformation info = new PlayerInformation (0, 0, PlayerInformation.PlayerState.MOVING);
+			List<byte> data = serializer.Serialize (info);
+
+			// Deserialize the result
+			PlayerInformation resultingInfo = serializer.Deserialize (data);
+
+			// Check the information is the same as the original
+			Assert.That(resultingInfo.Equals(info));
+		}
+	}
+}
+
