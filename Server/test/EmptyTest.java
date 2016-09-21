f94045acc8fa2d1d9d10e18dfc8cd24aefe13d9d
Matt F <matomatical@gmail.com>
2016-09-21 17:06:58 +1000

    fixed empty test problem i think

--- Server/test/EmptyTest.java
+++ Server/test/EmptyTest.java
@@ -1,6 +1,13 @@
+import org.junit.Test;
+
 /** EmptyTest just to put at end of list
  * @author Matt <farrugiam@student.unimelb.edu.au>
  */
 public class EmptyTest {
 
+	@Test
+	public void emptyTestShouldDoNothingButAlsoNotComplain(){
+		
+	}
+	
 }
