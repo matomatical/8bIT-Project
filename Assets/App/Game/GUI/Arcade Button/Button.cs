a3611c969f088e680602d7728e1fcb810383abd4
Matt F <matomatical@gmail.com>
2016-10-03 04:52:12 +1100

    fixed mobile controls

--- Assets/App/Game/GUI/Arcade Button/Button.cs
+++ Assets/App/Game/GUI/Arcade Button/Button.cs
@@ -13,24 +13,42 @@ using System.Collections;
 
 namespace xyz._8bITProject.cooperace {
 	public class Button : MonoBehaviour, IPointerUpHandler,
-			IPointerDownHandler {
+			IPointerDownHandler, IDragHandler {
 
 		private bool input = false;
 
-//		void Start(){
-//			SpriteRenderer
-//		}
 
+		/* Allow getting the button state. */
 		public bool Input(){
 			return input;
 		}
 
+
+		private Image image;
+		public Sprite buttonOff, buttonOn;
+
+		void Start(){
+
+			// get the image of the button child
+			image = transform.Find("button").GetComponent<Image>();
+
+			image.sprite = buttonOff;
+		}
+
+
+
+		public virtual void OnDrag(PointerEventData ped){
+			OnPointerDown (ped);
+		}
+
 		public virtual void OnPointerDown(PointerEventData ped) {
 			input = true;
+			image.sprite = buttonOn;
 		}
 
 		public virtual void OnPointerUp(PointerEventData ped) {
 			input = false;
+			image.sprite = buttonOff;
 		}
 	}
 }
