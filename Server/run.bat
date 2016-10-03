acc89843b9ca56a96d215a8ca2680feba05b0b67
Athir Saleem <noc7c9@gmail.com>
2016-10-04 05:10:30 +1100

    improved server logging

--- Server/run.bat
+++ Server/run.bat
@@ -8,11 +8,6 @@ if "%1"=="" (
     set port=%1
 )
 
-:: navigate to directory
-cd bin/
-
 echo starting server once on port %port%
 
-java -cp ".;../jar/boon.jar;../jar/hamcrest.jar;../jar/junit.jar" xyz._8bITProject.cooperace.leaderboards.Server %port%
-
-cd ../
+java -cp "./bin;./jar/boon.jar;./jar/hamcrest.jar;./jar/junit.jar" xyz._8bITProject.cooperace.leaderboards.Server %port%
--- Server/src/com/matomatical/util/Log.java
+++ Server/src/com/matomatical/util/Log.java
@@ -13,37 +13,66 @@ import java.util.Date;
 public abstract class Log {
 
 	// OPTIONS
-	
+
 	/** Path to logfile */
-	private static final String path = "logfile.txt";
-	
+	private static String path = "logfile.txt";
+
 	/** Overwrite existing logfile? */
-	private static final boolean overwrite = false;
-	
+	private static boolean overwrite = false;
+
+	/** Log to stdout as well? */
+	private static boolean logToStdout = true;
+
 	/** Format for displaying the current time in log messages */
 	private static SimpleDateFormat date = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss:SSS");
-	
+
 	// LOGFILE
-	
+
 	/** For writing to the logfile */
-	private static PrintWriter logfile = logfile();
-	private static PrintWriter logfile() {
-		try{
-			PrintWriter logfile = new PrintWriter(new FileWriter(path, !overwrite));
-			logfile.println("*** BEGIN LOG ***");
-			return logfile;
-		} catch (IOException e) {
-			throw new AntiException("Error opening logfile: " + e.getMessage());
+	private static PrintWriter writer;
+	private static PrintWriter getWriter() {
+		if (writer == null) {
+			try {
+				writer = new PrintWriter(new FileWriter(path, !overwrite));
+				writer.println("*** BEGIN LOG ***");
+			} catch (IOException e) {
+				throw new AntiException("Error opening logfile: " + e.getMessage());
+			}
+		}
+		return writer;
+	}
+
+	// PUBLIC METHODS
+
+	/** Allow changing location of logfile */
+	public static synchronized void setPath(String path) {
+		setPath(path, false);
+	}
+	public static synchronized void setPath(String path, boolean overwrite) {
+		Log.path = path;
+		Log.overwrite = overwrite;
+		if (writer != null) {
+			writer.close();
 		}
 	}
-	
-	// STATIC LOG MESSAGE
-	
+
+	/** Allow toggling stdout logging */
+	public static synchronized void setStdoutLogging(boolean value) {
+		Log.logToStdout = value;
+	}
+
 	/** Print a message to the logfile */
 	public static synchronized void log(String msg){
-		long t = Thread.currentThread().getId();
-		String time = date.format(new Date());
-		logfile.println("("+time+") thread " + t + ": " + msg);
-		logfile.flush();
+		msg = String.format("(%s) thread %s: %s",
+			date.format(new Date()), Thread.currentThread().getId(), msg);
+
+		if (logToStdout) {
+			System.out.println(msg);
+		}
+
+		writer = getWriter();
+		writer.println(msg);
+		writer.flush();
 	}
+
 }
