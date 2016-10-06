c26ff9c07498dedf9a6c9b2e7f63193af400e4f7
Matt F <matomatical@gmail.com>
2016-10-06 20:18:43 +1100

    keys bob up and down

--- Assets/App/Game/Obstacles/Key/BobUpAndDown.cs
+++ Assets/App/Game/Obstacles/Key/BobUpAndDown.cs
@@ -2,6 +2,10 @@
  * Key bobbing animation logic.
  *
  * Athir Saleem <isaleem@student.unimelb.edu.au>
+ * 
+ * Modified to use a sine curve
+ * 
+ * Matthew Farrugia-Roberts <farrugiam@student.unimelb.edu.au>
  *
  */
 
@@ -9,35 +13,47 @@ using UnityEngine;
 using System.Collections;
 
 namespace xyz._8bITProject.cooperace {
+	
 	public class BobUpAndDown : MonoBehaviour {
 
-		public float bobSpeed = 0.25f;
-		public float bobOffset = 0.3f;
+		/// How many seconds for a full cycle?
+		public float period = 2;
+		/// How far above/below the origin do we go?
+		public float amplitude = 0.2f;
 
-		float originY;
-		int bobDir;
+		/// Keep track of when we are in the cycle
+		private float t = 0;
+
+		/// the sprite to bob up and down
+		SpriteRenderer sprite;
+		/// where does this sprite's transform begin?
+		Vector3 origin;
 
 		void Start() {
-			// record original position
-			originY = transform.position.y;
-			bobDir = 1;
+
+			// link up components
+
+			sprite = GetComponentInChildren<SpriteRenderer> ();
+
+			// store the initial position
+
+			origin = sprite.transform.position;
 		}
 
 		void Update() {
-			// reverse direction if the object moves too far
-			if (transform.position.y > originY + bobOffset) {
-				bobDir = -1;
-			}
-			if (transform.position.y < originY) {
-				bobDir = 1;
-			}
 
-			// move the object up and down
-			transform.position = new Vector3(
-				transform.position.x,
-				transform.position.y + bobDir * Time.deltaTime * bobSpeed,
-				transform.position.z
-			);
+			// what's our new phase?
+
+			t += Time.deltaTime;
+			while (t > period) { t -= period; }
+
+			// calculate the relevant offset using Sin
+
+			float offset = amplitude * Mathf.Sin (2 * Mathf.PI * t / period);
+
+			// apply that offset
+
+			sprite.transform.position = origin + Vector3.up * offset;
 		}
 
 	}
