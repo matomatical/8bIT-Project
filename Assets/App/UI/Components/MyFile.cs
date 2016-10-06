85f898ad94fc620c0f0ab7e6f7dd2ed81559dd69
Li Cheng <lcheng3@student.unimelb.edu.au>
2016-10-06 14:24:00 +1100

    added class File skeleton

---
+++ Assets/App/UI/Components/MyFile.cs
@@ -0,0 +1,25 @@
+﻿using System.IO;
+using UnityEngine;
+using System.Collections;
+
+namespace xyz._8bITProject.cooperace.leaderboard {
+	class MyFile : MonoBehaviour {
+		void Start() {
+			StreamWriter w;
+			FileInfo f;
+			f = new FileInfo (Application.persistentDataPath + "//" + "testFile.txt");
+
+//			string aString[] = "Hello World!";
+//			File.WriteAllLines(Application.persistentDataPath, aString);
+
+			if (!f.Exists) {
+				w = f.CreateText ();
+			} else {
+				f.Delete ();
+				w = f.CreateText ();
+			}
+			w.WriteLine ("Hello World");
+			w.Close ();
+		}
+	}
+}
