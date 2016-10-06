14593cd13c5d13e026742067fcdd6686c1aa063c
Li Cheng <lcheng3@student.unimelb.edu.au>
2016-10-06 16:30:32 +1100

    working class File, contains read/write

--- Assets/App/UI/Components/MyFile.cs
+++ Assets/App/UI/Components/MyFile.cs
@@ -5,12 +5,18 @@ using System.Collections;
 namespace xyz._8bITProject.cooperace.leaderboard {
 	class MyFile : MonoBehaviour {
 		void Start() {
+			Write ("testFile.txt", "Hello Woorld");
+
+			Read("testFile.txt");
+
+		}
+
+		// Write a text/string into folder 'Application.persistantDataPath', which is 
+		// a public directory on IOS/Android device
+		static void Write(string filename, string text) {
 			StreamWriter w;
 			FileInfo f;
-			f = new FileInfo (Application.persistentDataPath + "//" + "testFile.txt");
-
-//			string aString[] = "Hello World!";
-//			File.WriteAllLines(Application.persistentDataPath, aString);
+			f = new FileInfo (Application.persistentDataPath + "//" + filename);
 
 			if (!f.Exists) {
 				w = f.CreateText ();
@@ -18,8 +24,15 @@ namespace xyz._8bITProject.cooperace.leaderboard {
 				f.Delete ();
 				w = f.CreateText ();
 			}
-			w.WriteLine ("Hello World");
+			w.WriteLine (text);
 			w.Close ();
+		}
+
+		// Read text contents from a file
+		static void Read(string filename) {
+			StreamReader r = File.OpenText(Application.persistentDataPath + "//" + "testFile.txt");
+			string data = r.ReadToEnd ();
+			r.Close ();
 		}
 	}
 }
