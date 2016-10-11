c0e0bade9f0db37dc270b1a71149f825e2883770
Samuel Beyer <sbeyer@student.unimelb.edu.au>
2016-10-11 16:20:56 +1100

    Added the abstract class to be used by obstacles which can have their state captured in a boolean value.

---
+++ Assets/App/Multiplayer/BoolObstacleInformation.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace xyz._8bITProject.cooperace.multiplayer
+{
+	public class BoolObstacleInformation
+	{
+		public readonly byte ID;		// ID to uniquley identify the obstacle
+		public readonly bool state;		// State of the obstacle
+
+		public BoolObstacleInformation (byte ID, bool state)
+		{
+			this.ID = ID;
+			this.state = state;
+		}
+
+		public override bool Equals(System.Object obj)
+		{
+			// If parameter is null return false.
+			if (obj == null)
+			{
+				return false;
+			}
+
+			// If parameter cannot be cast to Point return false.
+			BoolObstacleInformation info = obj as PlayerInformation;
+			if ((System.Object)info == null)
+			{
+				return false;
+			}
+
+			// Return true if the fields match:
+			return (info.ID == ID && info.state == state);
+		}
+
+		public bool Equals(BoolObstacleInformation info)
+		{
+			// If parameter is null return false:
+			if ((object)info == null)
+			{
+				return false;
+			}
+
+			// Return true if the fields match:
+			return (info.ID == ID && info.state == state);
+		}
+	}
+}
+
---
+++ Assets/App/Multiplayer/BoolObstacleSerializer.cs
@@ -0,0 +1,89 @@
+﻿/*
+ * The multiplayer serializer for Boolean state obstacles
+ * Used for representation of the state of the object as a list of bytes and updating the state of from a list of bytes.
+ *
+ * Mariam Shahid  < mariams@student.unimelb.edu.au >
+ * Sam Beyer     < sbeyer@student.unimelb.edu.au >
+*/
+
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace xyz._8bITProject.cooperace.multiplayer {
+	public abstract class BoolObstacleSerializer : MonoBehaviour, ISerializer<BoolObstacleInformation> {
+
+		public IUpdateManager updateManager;	// IUpdateManager to send updates to
+		private bool lastState;					// The last known state of this obstacle
+		private bool firstRun = true;			// Used to detect if HasChanged has been run before
+		private readonly int BYTE_TAKE_BIT = 7;	// The number of bits in a byte minus 1
+
+		void FixedUpdate () {
+			if (HasChanged ()) {
+				updateManager.SendObstacleUpdate (Serialize (new BoolObstacleInformation(ID, GetState ())));
+			}
+		}
+
+		/// <summary>
+		/// Should be implemented to return the state of the object being serialized
+		/// </summary>
+		/// <returns>The state of the obstacle being serialized.</returns>
+		public abstract bool GetState ();
+
+		public abstract void Notify (List<byte> message);
+
+		/// <summary>
+		/// Determines whether the obstacle has changed since last being called.
+		/// </summary>
+		/// <returns><c>true</c> if this obstacle has changed; otherwise, <c>false</c>.</returns>
+		public bool HasChanged () {
+
+			bool ret;		// the value to be returned
+			bool currState;	// the value of the current state
+
+			// If this is the first time HasChanged is being called, there is no last state to refer to.
+			if (firstRun) {
+				firstRun = false;
+				lastState = GetState ();
+				return true;
+			}
+
+			// If the last state is the same as the current state, return true, otherwise false
+			currState = GetState ();
+			ret = lastState != currState;
+
+			// Update the last seen state
+			lastState = currState;
+
+			return ret;	
+		}
+
+		public List<byte> Serialize (BoolObstacleInformation information)
+		{
+			List<byte> data = new List<byte> ();	// The list to store the update
+
+			// Add information about the state to the data
+			data.Add(information.state ? information.ID + 2^BYTE_TAKE_BIT : information.ID);
+
+			return data;
+		}
+
+		public BoolObstacleInformation Deserialize (List<byte> data)
+		{
+			byte info = data [0];	// Information stored in data
+			byte ID;				// ID of obstacle in the data
+			bool state;				// State of the obstacle in the data
+
+			// Deserialize
+			if (info > 127) {
+				ID = info - 128;
+				state = true;
+			} else {
+				ID = info;
+				state = false;
+			}
+				
+			return new BoolObstacleInformation (ID, state);
+		}
+	}
+}
