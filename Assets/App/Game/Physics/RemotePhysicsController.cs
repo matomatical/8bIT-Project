e8bde20a3815a25fd529f23c265094dafc045b7b
Matt F <matomatical@gmail.com>
2016-10-17 07:32:36 +1100

    implemented time-offset velocity lerping. check it out inside static record level game scene!

--- Assets/App/Game/Physics/RemotePhysicsController.cs
+++ Assets/App/Game/Physics/RemotePhysicsController.cs
@@ -1,6 +1,13 @@
 /*
  * Player 'two' controller, extending arcade physics to
  * follow state updates from a network or file
+ * 
+ * Smooths velocity of updates in the x direction. By delaying
+ * updates a small amount (offset), is able to lerp (linearly
+ * interpolate) between the previous state and the next one.
+ * Position is corrected after each frame is passed to make
+ * sure we don't accumulate error in position. When there are
+ * no updates, the most recent velocity is maintained.
  *
  * Matt Farrugia <farrugiam@student.unimelb.edu.au>
  * Sam Beyer <sbeyer@student.unimelb.edu.au>
@@ -8,100 +15,106 @@
  */
 
 using UnityEngine;
-using System.Collections;
-using System.Diagnostics;
+using System.Collections.Generic;
 
 namespace xyz._8bITProject.cooperace {
 
 	public class RemotePhysicsController : ArcadePhysicsController {
 
-		private Vector2 nextPosition;
-		private Vector2 nextVelocity;
 
-		private Vector2 previousPosition;
-		private Vector2 previousVelocity;
+		// how far back in time to lag? (default = 0.1 sec = 5 fixedupdates)
+		public float offset = 0.1f;
 
-		private bool positionApplied = false;
-		private bool velocityApplied = false;
+		class StateTime {
+			public Vector2 position, velocity;
+			public float time;
+			public bool pSet = false, vSet = false;
+			public StateTime(Vector2 x, Vector2 v){
+				this.time = Time.time;
+				this.position = x;
+				this.velocity = v;
+			}
+		}
 
-		private RollingAverage rollingAverage = new RollingAverage (100);
+		private Queue<StateTime> states = new Queue<StateTime>();
 
-		Stopwatch stopWatch = new Stopwatch ();
+		StateTime last, next;
 
 		protected override void Start(){
 
 			base.Start();
 
-			// Time.timeScale = 0.3f;
-			// Time.fixedDeltaTime *= Time.timeScale;
-
-			previousPosition = transform.position;
-			nextPosition = transform.position;
-
-			previousVelocity = Vector2.zero;
-			nextVelocity = Vector2.zero;
-
-			stopWatch.Start ();
-
+			last = new StateTime (GetPosition(), GetVelocity());
+			last.time = 0;
+			next = null;
 		}
 
 		public void SetState(Vector2 position, Vector2 velocity){
 
-			// Get the time since nextPosition and nextVelocity were last set
-			rollingAverage.Add (stopWatch.ElapsedMilliseconds);
-
-			// Restart the stop watch
-			stopWatch.Reset ();
-			stopWatch.Start ();
-
-			previousPosition = nextPosition;
-			nextPosition = position;
-			positionApplied = false;
-
-			previousVelocity = nextVelocity;
-			nextVelocity = velocity;
-			velocityApplied = false;
-		}
-
-		protected override void ChangePosition(ref Vector2 position){
-
-			if(!positionApplied){
-				position = previousPosition;
-				positionApplied = true;
-			}
+			states.Enqueue(new StateTime(position, velocity));
 		}
 
 		protected override void ChangeVelocity(ref Vector2 velocity){
 
-			Vector2 lerpedVel;
-			float timeToNext = rollingAverage.average;
-			UnityEngine.Debug.Log (string.Format ("Lerping from 0 to {0}ms", timeToNext));
+			// update the time
 
-			// Get the time since we got the last update
-			long elapsed = stopWatch.ElapsedMilliseconds;
+			float time = Time.time - offset;
 
-			// Only lerp if there is time between updates
-			if (timeToNext != 0) {
 
-				// Lerp between previous velocity and the next one
-				lerpedVel = Vector2.Lerp (previousVelocity, nextVelocity, elapsed / timeToNext);
+			if (next != null && time < next.time) {
+				
+				// we still have time!
 
-				// Update the velocity
-				velocity.x = lerpedVel.x;
+				float progress = (time - last.time) / (next.time - last.time);
+				velocity.x = Mathf.Lerp (last.velocity.x, next.velocity.x, progress);
 
-				if (!velocityApplied) {
-					velocity.y = previousVelocity.y;
-					velocityApplied = true;
+			} else {
+				
+				// time is already past next.time! let's advance to find a new next
+
+				while (next != null && next.time < time) {
+					last = next;
+					next = (states.Count > 0) ? states.Dequeue () : null;
+				}
+
+
+				// what if we're so far ahead that we're ahead of the
+				// latest update?
+
+				if (next == null) {
+					// do nothing until we have a new update:
+					next = (states.Count > 0) ? states.Dequeue () : null;
+
+				} else {
+					// we found a new 'next' in the while loop, successfully
+					// don't need to do anything, but this is the last case
+				}
+
+
+				// as soon as a new state becomes last, we should apply its
+				// vertical velocity (but let gravity take care of the rest!)
+
+				if(!last.vSet){
+					velocity.y = last.velocity.y;
+					last.vSet = true;
 				}
 			}
+		}
 
-			// Otherwise, just update the velocity
-			else {
-				if (!velocityApplied) {
-					velocity = previousVelocity;
-					velocityApplied = true;
-				}
+		public bool projecting;
+		protected override void ChangePosition(ref Vector2 position){
+
+			// as soon as a new state becomes last,
+			// we should correct our position
+
+			if(!last.pSet){
+				position = last.position;
+				last.pSet = true;
 			}
+
+			// last.position + (Time.time - offset - last.time) * last.velocity
+			// could be more accurate, but we'd lose collision prevention if we
+			// project and teleport to a non-safe position (into floor or wall)
 		}
 	}
 }
