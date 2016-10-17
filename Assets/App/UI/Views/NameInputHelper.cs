8dae6d0e5e56e85acfb879b586e93eaa27284561
Li Cheng <lcheng3@student.unimelb.edu.au>
2016-10-17 20:42:13 +1100

    comments added

--- Assets/App/UI/Views/NameInputHelper.cs
+++ Assets/App/UI/Views/NameInputHelper.cs
@@ -1,22 +1,45 @@
-﻿using UnityEngine;
+﻿/*
+ * Player inputs name logic
+ *
+ * Li Cheng <lcheng3@student.unimelb.edu.au>
+ * Athir Saleem <isaleem@student.unimelb.edu.au> 
+ *
+ */
+
+using UnityEngine;
 using UnityEngine.UI;
 using System.Collections;
 using xyz._8bITProject.cooperace.persistence;
 
 public class NameInputHelper : MonoBehaviour {
+
+	// valid alphabet
+
 	const string VALID_CHARS = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+
+	// valid character index in game alphabet 
+
 	int[] charIndices;
+
+	// three UI text elements
+
 	public Text[] textUI;
+
+	// real path stores the player name
+
 	private const string filename = "playerName.txt";
 
-
 	void Start() {
+		
 		charIndices = new int[3];
 
 		// read in existing name
+
 		string name = PersistentStorage.Read(filename);
 		 
-		// if it's valid name
+		// make sure it's a valid name
+		// check if letter not in game alphabet 
+
 		if (name.Length >= 3) {
 			for (int i = 0; i < 3; i++) {
 				if (VALID_CHARS.IndexOf (name [i]) == -1) {
@@ -27,17 +50,26 @@ public class NameInputHelper : MonoBehaviour {
 			}
 		}
 
-		UpdateTextUI ();
+		// update three UI text elements at one go
 
+		UpdateTextUI ();
 
 	}
 
+	/// <summary>
+	/// Increment the text UI element/box
+	/// </summary>
+	/// <param name="index">Index.</param>
 	public void Increment(int index) {
 		charIndices [index]++;
 		wrapAroundIndex (index);
 		UpdateTextUI ();
 	}
 
+	/// <summary>
+	/// Decrement the text UI element/box
+	/// </summary>
+	/// <param name="index">Index.</param>
 	public void Decrement(int index) {
 		charIndices [index]--;
 		wrapAroundIndex (index);
@@ -46,7 +78,7 @@ public class NameInputHelper : MonoBehaviour {
 
 	/// <summary>
 	/// Go around all valid characters. 
-	/// When the last character is reached, it starts from the beginning.
+	/// When the last character is reached, starts from the beginning.
 	/// </summary>
 	/// <param name="index">index</param>
 	void wrapAroundIndex(int index) {
@@ -60,7 +92,7 @@ public class NameInputHelper : MonoBehaviour {
 	/// <summary>
 	/// Convert 3 text UI elements to a string
 	/// </summary>
-	/// <returns>name</returns>
+	/// <returns>name of the player</returns>
 	public string GetName() {
 
 		return textUI[0].text + textUI[1].text + textUI[2].text;
@@ -77,7 +109,7 @@ public class NameInputHelper : MonoBehaviour {
 	}
 
 	/// <summary>
-	/// Save the file, and go back to mainmenu
+	/// Save the file - one functionality of the confirm button.
 	/// </summary>
 	public void ConfirmButton() {
 		PersistentStorage.Write (filename, GetName ());
