921a26c2d55704ab80aa75ab8723ebc6d5b041d8
Athir Saleem <noc7c9@gmail.com>
2016-10-19 12:49:12 +1100

    remade pause and disconnection uis

---
+++ Assets/App/UI/ViewControllers/PauseMenuController.cs
@@ -0,0 +1,32 @@
+﻿/*
+ * Pause menu logic.
+ *
+ * Athir Saleem <isaleem@student.unimelb.edu.au>
+ *
+ */
+
+using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+namespace xyz._8bITProject.cooperace.ui {
+	public class PauseMenuController : MonoBehaviour {
+
+		// ui elements
+		public GameObject OnScreenControls;
+		public GameObject PauseMenu;
+
+		// public method to handle resume button behaviour
+		public void ResumeButtonHandler() {
+			PauseMenu.SetActive(false);
+			OnScreenControls.SetActive(true);
+		}
+
+		// public method to handle exit button behaviour
+		public void ExitButtonHandler() {
+			SceneManager.Load("Main GUI");
+			UIHelper.LeaveRoom();	
+		}
+
+	}
+}
